8728018063b1387a5ecce3f87ea540936234e8e3
kanishk779 <kashyapamankumar54@gmail.com>
2021-02-19 23:08:58 +0530

    Initial message passed

--- ques2.erl
+++ ques2.erl
@@ -1,5 +1,5 @@
 -module(ques2).
--export([main/1, inputGraph/1, takeEdges/3, eachProcess/0]).
+-export([main/1, inputGraph/1, takeEdges/3, eachProcess/0, runProcess/6, createProcesses/3, splitVertices/5]).
 
 %% Edge_list is a dictionary Vertex -> [Edge], where Edge = {To, Weight}
 inputGraph(InF) ->
@@ -37,14 +37,19 @@ takeEdges(InF, Edge_list, Source) ->
 					takeEdges(InF, Edge_list2, Source)
 			end  % This is very important (learn how to use nested Case, otherwise u r doomed)
 	end.
-
+% Processes -> no. of processes , K ->, Vertices -> no. of vertices, 
+runProcess(Edge_list, Processes, K, Vertices, Status, Root_pid) ->
+	io:format("~w ~n", [1]),
+	1.
 eachProcess() ->
+	receive {Edge_list, Processes, K, Vertices, Status, Root_pid} -> ok end,
+	runProcess(Edge_list, Processes, K, Vertices, Status, Root_pid),
 	1.
 % dijkstra() ->
 
 
 createProcesses(Num_Id, Curr, Processes) ->
-	Pid = spawn('ques2', eachProcess, []),
+	Pid = spawn(?MODULE, eachProcess, []),
 	Num_Id1 = dict:store(Curr, Pid, Num_Id),
 	if
 		Curr == Processes ->
@@ -53,11 +58,27 @@ createProcesses(Num_Id, Curr, Processes) ->
 			createProcesses(Num_Id1, Curr+1, Processes)
 	end.
 
+splitVertices(VertexDict, L, N, Curr, Processes) ->
+	if Curr == Processes ->
+		VertexDict1 = dict:store(Curr, L, VertexDict),
+		VertexDict1;
+	true ->
+		{L1, L2} = lists:split(N, L),
+		VertexDict1 = dict:store(Curr, L1, VertexDict),
+		splitVertices(VertexDict1, L2, N, Curr+1, Processes)
+	end.
+	
 main([InF, OutF]) ->
 	{Edge_list, Source, Processes, Vertices, Edges} = inputGraph(InF),
 	N = dict:new(),
-	Num = dict:store(1, self(), N),
-	Num_Id = createProcesses(Num, 1, Processes),
+	% Num_Id is the dictionary which maps process number to process PID
+	Num_Id = createProcesses(N, 2, Processes),
+	Id_list = dict:to_list(Num_Id),
 	{ok, Out} = file:open(OutF, [write]), % open the output file
-	io:format(Out, "~w~n", Num_Id),
+	% io:format(Out, "~w~n", Num_Id),
+	io:format(Out, "~w~n", [Id_list]),
+	Process_Nums = lists:seq(1, Processes),
+	Status = lists:foreach(fun(Key) -> {if Key == 1 -> 0; true -> infinity end, not_visited} end, Process_Nums),
+	Worker = fun({K, V}) -> V ! {Edge_list, Processes, K, Vertices, Status, self()} end,
+	lists:foreach(Worker, Id_list),
 	file:close(Out).
